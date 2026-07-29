2fc6a999a42fadf3ca121c50d1c7c250d614e785
slyrzzz <jarc.01@outlook.com>
2026-07-29 17:19:03 -0500

    fix de fondos negros, textos negros, armonia de textos y fondos

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,10 +1,10 @@
 import '@payloadcms/next/css';
-import '../globals.css';
 import configPromise from '@payload-config';
 import { RootLayout } from '@payloadcms/next/layouts';
 import React from 'react';
 import { importMap } from './importMap';
 import { payloadServerFunction } from './actions';
+
 
 type Args = {
   children: React.ReactNode;
